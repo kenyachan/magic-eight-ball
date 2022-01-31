e2e2b84335488c031e86372d07338a2a1499d622
Kenya Chan <kenya.chan@gmail.com>
2022-01-31 21:38:06 +1100

    Change "response" to "fortune"

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 
-let shakeButton = document.getElementById("shakeButton");
-let responseText = document.getElementById("responseText");
+const shakeButton = document.getElementById("shakeButton");
+const fortuneText = document.getElementById("fortuneText");
 
-shakeButton.addEventListener("click", shakeResponse);
-responseText.addEventListener("animationend", shakeResponse);
+// add event listeners to elements for controlling the jitters
+shakeButton.addEventListener("click", toggleFortuneTextShake);
+fortuneText.addEventListener("animationend", toggleFortuneTextShake);
 
-async function getResponse() {
-    // Get eightball responses array
-    const requestURL = "../eight_ball_responses.json";
-    const request = new Request(requestURL);
-    const responseData = await fetch(request);
-    const responses = await responseData.json();
-
-    // Random number from 0-7 corresponding to response array positions
-    let randomNumber = Math.floor(Math.random() * 8);
-    // The response that will be displayed
-    let eightBallResponse = responses[randomNumber];
-
-    document.getElementById("responseText").innerHTML = eightBallResponse;
+function toggleFortuneTextShake() {
+    document.getElementById("fortuneText").classList.toggle("shake");
 }
 
-function shakeResponse() {
-    document.getElementById("responseText").classList.toggle("shake");
+async function getFortune() {
+    // Get eightball fortune array
+    const requestURL = "../eight_ball_fortunes.json";
+    const request = new Request(requestURL);
+    const fortuneData = await fetch(request);
+    const fortunes = await fortuneData.json();
+
+    // Random number from 0-7 corresponding to fortune array positions
+    let randomNumber = Math.floor(Math.random() * 8);
+    // The fortune that will be displayed
+    let eightBallFortune = fortunes[randomNumber];
+
+    fortuneText.innerHTML = eightBallFortune;
 }
